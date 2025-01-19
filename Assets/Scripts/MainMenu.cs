1f0205c29923861ba3a45c232ada035426f29d40
NitoVal <nitoval360@gmail.com>
2025-01-19 18:46:10 -0500

    Added Player HUD

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void StartGame()
+    private void Update()
+    {
+        if (Input.anyKey)
+            StartGame();
+    }
+
+    private void StartGame()
     {
         SceneManager.LoadScene(1);
     }
