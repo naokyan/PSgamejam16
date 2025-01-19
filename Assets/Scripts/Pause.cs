a683d711e08c76dbecb6d2896bfb65336f44c19e
NitoVal <nitoval360@gmail.com>
2025-01-19 01:23:57 -0500

    Updated Pause & Main Menu

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,11 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
+    private bool _isPaused;
+    private bool _isAnimating;
+    
+    public GameObject PauseUI;
+    public RectTransform rectTransform;
+    
+    [Header("Buttons")]
     public Button musicButton;
     public Button soundButton;
     
@@ -16,16 +25,47 @@ public class Pause : MonoBehaviour
     [Header("Sound")]
     public Sprite soundOff;
     public Sprite soundOn;
-    
+
+    private void Start()
+    {
+        rectTransform.localScale = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            PauseGame();
+        }
+    }
+
     public void PauseGame()
     {
-        
+        if (_isAnimating) return;
+        _isAnimating = true;
+        if (_isPaused)
+        {
+            _isPaused = false;
+            Time.timeScale = 1f;
+            rectTransform.DOScale(Vector3.zero, .5f).SetEase(Ease.OutBack).OnComplete(() =>
+            {
+                _isAnimating = false;
+            });
+        }
+        else
+        {
+            _isPaused = true;
+            rectTransform.DOScale(Vector3.one, .5f).SetEase(Ease.OutBack).OnComplete(() =>
+            {
+                _isAnimating = false;
+                Time.timeScale = 0f;
+            });
+        }
     }
 
     public void ExitToMainMenu()
     {
-        //TODO SET MAIN MENU SCENE
-        //SceneManager.LoadScene(0);
+        SceneManager.LoadScene(0);
     }
 
     public void ToggleMusic()
@@ -36,7 +76,7 @@ public class Pause : MonoBehaviour
         }
         else
         {
-            
+            //soundButton.GetComponent<Image>().sprite = musicOn;
         }
     }
 
@@ -48,7 +88,7 @@ public class Pause : MonoBehaviour
         }
         else
         {
-            
+            //soundButton.GetComponent<Image>().sprite = soundOn;
         }
     }
 }
