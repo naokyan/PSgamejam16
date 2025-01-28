e4d90df45e80d46636f5583f9d74b5ca0ac2a1a3
NitoVal <nitoval360@gmail.com>
2025-01-28 16:18:49 -0500

    added game over ui

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public RectTransform GameOverPanel;
+    
+    private void Start()
+    {
+        GameOverPanel.localScale = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (Player.Instance.IsDead())
+        {
+            GameOverPanel.DOScale(Vector3.one, .5f).SetEase(Ease.OutBack);
+        }
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
